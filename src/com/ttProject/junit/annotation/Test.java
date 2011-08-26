6ac4de72122b26a537ccaf85b0d345383c1eafe2
Toda Takahiko <poepoemix@hotmail.com>
2011-08-26 20:21:50 +0900

    シングルトンに対応。privateメソッドにも対応させるつもり。 あとは、特殊パラメータ、特殊返答も作成しておく必要あり。

--- src/com/ttProject/junit/annotation/Test.java
+++ src/com/ttProject/junit/annotation/Test.java
@@ -3,6 +3,11 @@ package com.ttProject.junit.annotation;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/*
+ * テスト用のアノーテーション
+ * paramsは入力配列
+ * assumeはあるべき結果
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
 	String[] params();
