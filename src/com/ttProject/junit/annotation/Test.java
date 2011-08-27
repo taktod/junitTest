4dd44ba920b687cf72d54be87a03d0ff92630915
Toda Takahiko <poepoemix@hotmail.com>
2011-08-27 11:19:01 +0900

    配列のみサポートしてみた、テスト段階。

--- src/com/ttProject/junit/annotation/Test.java
+++ src/com/ttProject/junit/annotation/Test.java
@@ -6,15 +6,16 @@ import java.lang.annotation.RetentionPolicy;
 /*
  * テスト用のアノーテーション
  * paramsは入力配列
- * "@string[abc][test][gesg][trsy]" hashSetにする。型は自分で宣言する必要あり。(省略時はString強制)
- * "$string@string[a->test][b->hello]" hashMapにする。型は自分で宣言する必要あり。(省略時はString強制 keyもvalも)
- * つかえる型はchar byte short int long double float string
+ * "string[abc][test][gesg][trsy]" hashSetにする。型は自分で宣言する必要あり。(省略時はString強制)
+ * "string->string[a->test][b->hello]" hashMapにする。型は自分で宣言する必要あり。(省略時はString強制 keyもvalも)
+ * つかえる型はboolean char byte short int long double float string
  * あらかじめMapにデータをセットしておいて、そのデータを参照することも可
  * "#data"この場合はsetUpで所定のHashMapにデータを入れる必要あり。(できた)
  * 使える型は
  * *[は入力文字に指定不可
  * assumeはあるべき結果
  * @none 応答チェックなし。(できた)
+ * @ok 応答チェックなし。例外はエラーにする。(できた)
  * @dump 応答データを標準出力にだす。チェックはしない。(できた)
  * @Exception 例外(できた)
  */
--- src/com/ttProject/junit/library/MethodChecker.java
+++ src/com/ttProject/junit/library/MethodChecker.java
@@ -1,5 +1,6 @@
 package com.ttProject.junit.library;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -15,6 +16,7 @@ import com.ttProject.junit.annotation.Test;
 
 import static org.junit.Assert.*;
 
+@SuppressWarnings("unused")
 public class MethodChecker {
 	/** チェック対象クラス */
 	private Set<Class<?>> classSet = null;
@@ -76,6 +78,9 @@ public class MethodChecker {
 				}
 				else {
 					// 一般関数 (new Instanceの部分初期か方法が指定されている場合はそっちにあわせる。)
+					for(Object obj : dataList) {
+						System.out.println(obj);
+					}
 					ret = method.invoke(getClassInstance(cls), dataList.toArray());
 				}
 				method.setAccessible(access);
@@ -87,7 +92,7 @@ public class MethodChecker {
 				if(assume.equals("@dump")) {
 					dumpAll(ret);
 				}
-				else if(assume.equals("@none") || assume.equals(ret.toString())) {
+				else if(assume.equals("@none") || assume.equals("@ok") || assume.equals(ret.toString())) {
 					System.out.println("...passed...");
 				}
 				else {
@@ -138,47 +143,188 @@ public class MethodChecker {
 		 * ...
 		 * 配列の扱いもなんとかしないといけない。
 		 */
+		// nullチェック
 		if(obj == null || "null".equals(obj)) {
 			return null;
 		}
+		// dataMapに設定されているデータ
 		if(obj.startsWith("#")) {
 			return dataMap.get(obj.substring(1));
 		}
+		// 文字列
 		if(type == String.class) {
 			return obj;
 		}
+		// int
 		if(type == int.class || type == Integer.class) {
 			return new Integer(obj);
 		}
+		// long
 		if(type == long.class || type == Long.class) {
 			return new Long(obj);
 		}
+		// float
 		if(type == float.class || type == Float.class) {
 			return new Float(obj);
 		}
+		// double
 		if(type == double.class || type == Double.class) {
 			return new Double(obj);
 		}
+		// short
 		if(type == short.class || type == Short.class) {
 			return new Short(obj);
 		}
+		// byte
 		if(type == byte.class || type == Byte.class) {
 			return new Byte(obj);
 		}
+		// char
 		if(type == char.class || type == Character.class) {
 			return obj.charAt(0);
 		}
+		// boolean
 		if(type == boolean.class || type == Boolean.class) {
 			return new Boolean(obj);
 		}
-		if(type == Set.class) {
+		// 配列
+		if(type.isArray()) {
+			return makeArray(type, obj);
 		}
-		if(type == Map.class) {
-			
+		// リスト
+		if(List.class.isAssignableFrom(type)) {
+		}
+		// セット
+		if(Set.class.isAssignableFrom(type)) {
+		}
+		// マップ
+		if(Map.class.isAssignableFrom(type)) {
 		}
 		// それ以外のクラスの場合、そこにInitのアノーテーション指定があるなら、その初期化方法で、それ以外の場合はデフォルトコンストラクタを利用して。おく。
+		return getClassInstance(type);
+	}
+	private Object makeArray(Class<?> resultType, String obj) {
+		if(!obj.contains("[")) {
+			return null;
+		}
+		String target = resultType.getName();
+		String[] type = obj.split("\\[", 2);
+		Constructor<?> construct = getElementType(type[0]);
+		String[] array = getElementArray(obj);
+		try {
+			if("[Z".equals(target)) {
+				boolean[] result = new boolean[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = new Boolean(array[i]);
+				}
+				return result;
+			}
+			if("[C".equals(target)) {
+				char[] result = new char[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = array[i].charAt(0);
+				}
+				return result;
+			}
+			if("[B".equals(target)) {
+				byte[] result = new byte[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = new Byte(array[i]);
+				}
+				return result;
+			}
+			if("[S".equals(target)) {
+				short[] result = new short[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = new Short(array[i]);
+				}
+				return result;
+			}
+			if("[I".equals(target)) {
+				int[] result = new int[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = new Integer(array[i]);
+				}
+				return result;
+			}
+			if("[J".equals(target)) {
+				long[] result = new long[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = new Long(array[i]);
+				}
+				return result;
+			}
+			if("[F".equals(target)) {
+				float[] result = new float[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = new Float(array[i]);
+				}
+				return result;
+			}
+			if("[D".equals(target)) {
+				double[] result = new double[array.length];
+				for(int i = 0;i < array.length;i ++) {
+					result[i] = new Double(array[i]);
+				}
+				return result;
+			}
+		}
+		catch (Exception e) {
+		}
+		/*
+		 * とりあえず文字列であると仮定しておく。
+		 */
+		return array;
+	}
+	private Constructor<?> getElementType(String target) {
+		try {
+			if("boolean".equals(target)) {
+				return Boolean.class.getConstructor(String.class);
+			}
+			if("char".equals(target)) {
+				return Character.class.getConstructor(String.class);
+			}
+			if("byte".equals(target)) {
+				return Byte.class.getConstructor(String.class);
+			}
+			if("short".equals(target)) {
+				return Short.class.getConstructor(String.class);
+			}
+			if("int".equals(target)) {
+				return Integer.class.getConstructor(String.class);
+			}
+			if("long".equals(target)) {
+				return Long.class.getConstructor(String.class);
+			}
+			if("float".equals(target)) {
+				return Float.class.getConstructor(String.class);
+			}
+			if("double".equals(target)) {
+				return Double.class.getConstructor(String.class);
+			}
+		}
+		catch (Exception e) {
+		}
 		return null;
-//		return getClassInstance(type);
+	}
+	private String[] getElementArray(String target) {
+		// 文字列を割っておく。
+		String[] result;
+		String last;
+		int num;
+		if(!target.contains("[")) { // セパレータが存在しないので、分割できない。
+			return null;
+		}
+		result = target.split("\\[",2)[1].split("\\]\\[", 0);
+		num = result.length - 1;
+		last = result[num];
+		if(last.contains("]")) {
+			result[num] = last.split("\\]", 2)[0];
+		}
+		else {
+			result[num] = null;
+		}
+		return result;
 	}
 	/**
 	 * 対象クラスのデフォルトインスタンスを生成する。
@@ -189,13 +335,21 @@ public class MethodChecker {
 		Init init = null;
 		Boolean access = null;
 		Constructor<?> constructor = null;
+		try {
+			constructor = cls.getDeclaredConstructor();
+		}
+		catch (Exception e) {
+		}
 		// まずコンストラクタにアノーテーションがついているかチェック。
 		for(Constructor<?> construct : cls.getDeclaredConstructors()) {
-			constructor = construct;
 			init = construct.getAnnotation(Init.class);
 			if(init != null) {
+				constructor = construct;
 				break;
 			}
+		}
+		if(constructor == null) {
+			return null;
 		}
 		access = constructor.isAccessible();
 		constructor.setAccessible(true);
